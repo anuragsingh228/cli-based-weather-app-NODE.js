1e8b048e7067df725184abf7904b360bf6578b31
Anurag Singh <33912858+anuragsingh228@users.noreply.github.com>
2019-01-10 21:53:51 +0530

    Update geocode.js

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -3,7 +3,7 @@ const request = require('request');
 var geocodeAddress = (address, callback) => {
     var encodedAddress = encodeURIComponent(address);
     request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDtkpKQCK8LB0eAVPkuCgSQQ_P_bvsluLw`,
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=xxxxxxxxxxxxxxxxxxxx`,
         json: true
     }, (error, response, body) => {
         if (error) {
